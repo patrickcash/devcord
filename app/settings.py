33758efe0f3012b307dcc2150f014a87489965ac
Patrick Cash <patrickcash@gmail.com>
2023-02-01 14:47:59 -0500

    update allowed host for heroku

--- app/settings.py
+++ app/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = get_env_value('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['devcord-pc.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['devcord.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
